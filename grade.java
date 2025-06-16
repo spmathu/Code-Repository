dab0b132cb59c81538851793405d80061d452998
spmathu <mathumozhisp@gmail.com>
2025-06-16 17:36:17 +0530

    Add files via upload

---
+++ grade.java
@@ -0,0 +1,12 @@
+class grade {
+    public static void main(String[] args) {
+        int mark=60;
+        if(mark>=75){
+            System.out.println("grade A"); }
+            else if(mark>=60){
+                System.out.println("grade B");}
+                else{
+                    System.out.println("grade C");}
+    }
+}
+       
