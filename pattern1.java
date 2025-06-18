bbb59a86ce84c7b5f45ca4d19e74d9eecede9986
spmathu <mathumozhisp@gmail.com>
2025-06-18 21:19:48 +0530

    Update pattern1.java

--- pattern1.java
+++ pattern1.java
@@ -1,7 +1,7 @@
 public class pattern1 {
     public static void main(String[] args) {
         for (int i=1; i <=4; i++) {
-            for(int j=1;j<=i-1;j++) {
+            for(int j=1;j<=i+1;j++) {
                 System.out.print("*");
             }
             System.out.println();
