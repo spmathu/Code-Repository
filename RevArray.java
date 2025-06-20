d827663edbedef061f7aa7b0c62ee5e239b57e2c
spmathu <mathumozhisp@gmail.com>
2025-06-20 13:36:54 +0530

    Add files via upload

---
+++ RevArray.java
@@ -0,0 +1,15 @@
+public class RevArray {
+    public static void main(String[] args) {
+        int[] arr = {4,16,1,7,5};
+        for(int i=0;i<arr.length;i++){
+         System.out.print(arr[i]);
+        }
+        System.out.println();
+        for(int i=4;i>=0;i--){
+         System.out.println(arr[i]);
+
+
+    }
+    
+}
+}
