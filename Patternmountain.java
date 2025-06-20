e75f05660b2312f4fa275b8e826ecd3f415e5623
spmathu <mathumozhisp@gmail.com>
2025-06-20 14:04:04 +0530

    Add files via upload

---
+++ Patternmountain.java
@@ -0,0 +1,20 @@
+public class Patternmountain {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 1; i++) {
+            System.out.print("   ");
+            System.out.print("*");
+            System.out.println();
+        }
+        for(int i=1;i<=2;i++){
+            System.out.print("  ");
+            System.out.print("*");
+            
+        }
+        System.out.println();
+         for(int i=1;i<=3;i++){
+            System.out.print(" ");
+            System.out.print("* ");
+         }
+    }
+    
+}
