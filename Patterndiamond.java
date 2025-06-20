fdaafb76a445ce109108cd2cff3c189299431335
spmathu <mathumozhisp@gmail.com>
2025-06-20 14:04:26 +0530

    Add files via upload

---
+++ Patterndiamond.java
@@ -0,0 +1,31 @@
+public class Patterndiamond {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 1; i++) {
+            System.out.print("     ");
+            System.out.print("*");
+        }
+        System.out.println();
+        for(int i=1;i<=2;i++){
+            System.out.print("   ");
+            System.out.print("*");
+               
+        }
+        System.out.println();
+         for(int i=1;i<=3;i++){
+            System.out.print("  ");
+            System.out.print("*");
+         }
+         System.out.println();
+          for(int i=1;i<=2;i++){
+            System.out.print("   ");
+            System.out.print("*");
+         }
+         System.out.println();
+         for (int i = 1; i <= 1; i++) {
+            System.out.print("     ");
+            System.out.print("*");
+            
+        }
+    }
+    
+}
